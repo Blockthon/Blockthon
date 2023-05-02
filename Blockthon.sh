6e8b66a4d9024e986402a7861b2aa24a919ba554
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2023-05-02 17:11:58 +0300

    Blockthon (BASH) 1.0.3
    Blockthon for Bash Language

---
+++ Blockthon.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+PrivateKey() {
+    hexdump -n 32 -e '8/4 "%08X" 1 "\n"' /dev/random
+}
+
+PrivateKey_To_Dec() {
+    printf "%d" "0x$1"
+}
+
+PrivateKey_To_Bytes() {
+    echo -n $1 | xxd -r -p
+}
+
+Bytes_To_Mnemonic() {
+    mnemonic=$(python3 -c "from mnemonic import Mnemonic; print(Mnemonic('english').to_mnemonic('$1'))")
+    echo $mnemonic
+}
+
+Bytes_To_PrivateKey() {
+    echo -n $1 | xxd -p -c 1000
+}
+
+PrivateKey_To_Mnemonic() {
+    Byte_String=$(PrivateKey_To_Bytes $1)
+    mnemonic=$(python3 -c "from mnemonic import Mnemonic; print(Mnemonic('english').to_mnemonic('$Byte_String'))")
+    echo $mnemonic
+}
+
+Bytes_To_WIF() {
+    if [ "$2" = true ]; then
+        wif=$(python3 -c "from bit import Key; from bit.format import bytes_to_wif; print(bytes_to_wif('$1', compressed=True))")
+    else
+        wif=$(python3 -c "from bit import Key; from bit.format import bytes_to_wif; print(bytes_to_wif('$1'))")
+    fi
+    echo $wif
+}
+
+Balance() {
+    req=$(curl -s "https://bitcoin.atomicwallet.io/api/v2/address/$1")
+    balance=$(echo $req | python3 -c "import sys, json; print(json.load(sys.stdin)['balance'])")
+    echo $balance
+}
