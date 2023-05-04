f1b299bf3379ea63c972c0e68da08fd0b0a97d67
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2023-05-04 22:55:07 +0300

    Update AddrFromHex_CheckBalance.py

--- example/AddrFromHex_CheckBalance.py
+++ example/AddrFromHex_CheckBalance.py
@@ -1,17 +1,12 @@
-from Blockthon import PrivateKey, Addr_From_PrivateKey, Balance
+from Blockthon import PrivateKey, Addr_From_PrivateKey
 
+# Generate Private Key Hex Random
+key = PrivateKey()
+# Generated Compressed Address From Private Key Hex (key)
+compress_addr = Addr_From_PrivateKey(key, compress=True)
+# Generated un Compressed Address From Private Key Hex (key)
+uncompress_addr = Addr_From_PrivateKey(key)
 
-z = 0
-f = 0
-while True:
-    z += 1
-    key = PrivateKey()
-    caddr = Addr_From_PrivateKey(key, compress=True)
-    uaddr = Addr_From_PrivateKey(key)
-    cbal = Balance(caddr)
-    ubal = Balance(uaddr)
-    if cbal != '0' or ubal != '0':
-        f += 1
-        open("found.txt", "a").write(f"{caddr} : {cbal}\n{uaddr} : {ubal}\n{key}\n")
-    else:
-        print(f"[{z}] # Found:{f} > {caddr} : {cbal} # {uaddr} : {ubal}")
+print("Compressed Address:", compress_addr)
+print("unCompressed Address:", uncompress_addr)
+print("Private Key:", key)
