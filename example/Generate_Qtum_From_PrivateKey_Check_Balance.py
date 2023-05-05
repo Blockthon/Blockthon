a4280e1d80850de07f83d99adc8285c98b7feaf5
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2023-05-05 19:38:24 +0300

    Create Generate_Qtum_From_PrivateKey_Check_Balance.py

---
+++ example/Generate_Qtum_From_PrivateKey_Check_Balance.py
@@ -0,0 +1,17 @@
+import Blockthon
+
+z = 0
+while True:
+    z += 1
+    # Generate Private Key Hex (Random)
+    PrivateKey = Blockthon.PrivateKey()
+    # Mnemonic_str = Blockthon.PrivateKey_To_Mnemonic(PrivateKey)
+    # Generated Address From Private Key HEX
+    Addr = Blockthon.QTUM_From_PrivateKey(PrivateKey)
+    # Check Value Balance
+    balance = Blockthon.Balance_Qtum(Addr)
+    # ------------------------------------------------------------------------
+    print('Total:', z, 'Address:', Addr, 'Balance:', int(balance) / 1000000000)
+    print('PrivateKey:', PrivateKey)
+    print('-' * 66)
+
